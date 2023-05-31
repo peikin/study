cd464623d4718fe22072bf8532a1fbc5672f7781
BomiKim <93rlaqhal@gmail.com>
2023-05-31 17:55:00 +0900

    feat: 허용되지 않은 확장자인 경우 팝업 추가

--- src/components/review/FileUpload.tsx
+++ src/components/review/FileUpload.tsx
@@ -1,11 +1,13 @@
 import CameraAltIcon from '@mui/icons-material/CameraAlt'
-import { Box, IconButton, Typography } from '@mui/material'
+import { Box, IconButton, Modal, Typography } from '@mui/material'
 import { useDropzone, FileWithPath, FileRejection } from 'react-dropzone'
 import { useEffect, useState } from 'react'
 import CloseIcon from '@mui/icons-material/Close'
+import { ErrorText } from '../../constants/error'
 
 const FileUpload = () => {
   const [files, setFiles] = useState<FileWithPath[]>([])
+  const [rejectFileList, setRejectFileList] = useState<FileRejection[]>([])
 
   /**
    * 파일 업로드 에러 핸들링
@@ -13,6 +15,8 @@ const FileUpload = () => {
   const handleUploadError = (fileRejections: FileRejection[]) => {
     if (fileRejections.length > 5) {
       alert('5개 이상 업로드 할 수 없습니다.')
+    } else {
+      setRejectFileList(fileRejections)
     }
   }
 
@@ -57,47 +61,79 @@ const FileUpload = () => {
   }, [])
 
   return (
-    <Box sx={{ pt: 2 }}>
-      <div {...getRootProps()} className="dnd-wrapper">
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <div className="loading">
-            <Typography variant="h6">여기에 파일을 드롭하세요.</Typography>
-          </div>
-        ) : (
-          <div className="upload-wrapper">
-            <div className="upload-btn">
-              <CameraAltIcon color="primary" aria-label="upload picture" />
+    <>
+      <Box sx={{ pt: 2 }}>
+        <div {...getRootProps()} className="dnd-wrapper">
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <div className="loading">
+              <Typography variant="h6">여기에 파일을 드롭하세요.</Typography>
             </div>
-            {!!files.length && (
-              <ul className="preview-list">
-                {files.map((file, index) => (
-                  <li key={index}>
-                    <div className="img-wrapper">
-                      <img
-                        key={file.name}
-                        src={URL.createObjectURL(file)}
-                        alt={file.name}
-                        width="100"
-                      />
-                    </div>
-                    <IconButton
-                      aria-label="delete"
-                      onClick={(e) => {
-                        deleteFile(index)
-                        e.stopPropagation()
-                      }}
-                    >
-                      <CloseIcon />
-                    </IconButton>
+          ) : (
+            <div className="upload-wrapper">
+              <div className="upload-btn">
+                <CameraAltIcon color="primary" aria-label="upload picture" />
+              </div>
+              {!!files.length && (
+                <ul className="preview-list">
+                  {files.map((file, index) => (
+                    <li key={index}>
+                      <div className="img-wrapper">
+                        <img
+                          key={file.name}
+                          src={URL.createObjectURL(file)}
+                          alt={file.name}
+                          width="100"
+                        />
+                      </div>
+                      <IconButton
+                        aria-label="delete"
+                        onClick={(e) => {
+                          deleteFile(index)
+                          e.stopPropagation()
+                        }}
+                      >
+                        <CloseIcon />
+                      </IconButton>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          )}
+        </div>
+      </Box>
+      {!!rejectFileList.length && (
+        <Modal
+          open={!!rejectFileList.length}
+          onClose={() => setRejectFileList([])}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box className="modal-content">
+            <p>
+              업로드에 실패했습니다.
+              <br />
+              아래 내용을 확인해주세요.
+            </p>
+            <ul className="reject-list">
+              {rejectFileList.map(({ file, errors }) => {
+                return (
+                  <li key={file.name}>
+                    {file.name}
+                    <ul>
+                      {errors.map((e) => (
+                        <li key={e.code}>{ErrorText[e.code]}</li>
+                      ))}
+                    </ul>
                   </li>
-                ))}
-              </ul>
-            )}
-          </div>
-        )}
-      </div>
-    </Box>
+                )
+              })}
+            </ul>
+          </Box>
+        </Modal>
+      )}
+    </>
   )
 }
 
---
+++ src/constants/error.ts
@@ -0,0 +1,3 @@
+export const ErrorText: { [key: string]: string } = {
+  'file-invalid-type': '파일 확장자는 ".jpg, .jpeg, .png" 만 가능합니다.',
+}
